0a1ead8cb7f18396285f73c83d24e0e918f99090
Francisco <p.lemus.fm@gmail.com>
2019-02-07 10:33:00 -0600

    added custom string support

--- week1/HW1.rb
+++ week1/HW1.rb
@@ -3,12 +3,14 @@ puts " First exercise"
 
 puts "How many times?"
 repetitions = gets.to_i
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
+#alphabet = 'abcdefghijklmnopqrstuvwxyz'
+puts "String to permute:"
+alphabet = gets.chomp
 
 for i in (1 .. repetitions)
 	res = alphabet.chars.to_a.repeated_permutation(i).to_a
 	res.each{
 	| elem | elem.each{ | c | print c};
-	print " "
-}
+	print " "}
+puts
 end
